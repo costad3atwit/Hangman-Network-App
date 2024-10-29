49589cd9286faff3e69181717773649ec8e4b76b
Kyle Ragbir <ragbirk@wit.edu>
2024-10-29 16:31:17 -0400

    made it so player1 and player2 connect to in hangman.py. Server.py is not needed anymore.

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask import request 
 #pip install flask-socketio
 #to install line 5 copy the above command into the terminal
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 # Create a Flask app instance
 app = Flask(__name__)
@@ -12,6 +12,8 @@ app.config['SECRET_WORD'] = "secret!"
 socketio = SocketIO(app)
 # Define a route for the root URL ('/')
 
+player_count=0
+
 @app.route('/')
 def hello_world():
     message = "Hangman App! :P"
@@ -19,6 +21,18 @@ def hello_world():
     return render_template('index.html',  
                            message=message) 
 
+@socketio.on('connect')
+def handle_connect():
+    global player_count
+    player_count += 1
+
+    if player_count == 1:
+        print("Player 1 joined")
+        send("Player 1 joined", broadcast=True)
+    elif player_count == 2:
+        print("Player 2 joined")
+        send("Player 2 joined", broadcast=True)
+        
 #The below code block is a listener from the client
 #to await a role selection. There are two buttons
 #shown in index.html which send this information to the server.
