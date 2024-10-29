2281c124ef223d1deecd5d4729e25184f68a1738
ragbirkatwit <144716727+ragbirkatwit@users.noreply.github.com>
2024-10-29 10:40:21 -0400

    made player1 and player2 able to connect to 1 server

--- Server.py
+++ Server.py
@@ -13,17 +13,6 @@ def handle_client(client_socket, client_address):
     elif player_id == 2:
         print("Player 2 joined.")
     
-    while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                break
-            print(f"Received from {client_address}: {message}")
-        except:
-            break
-    client_socket.close()
-    print(f"Connection from {client_address} has been closed.")
-
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("0.0.0.0", 9999))
